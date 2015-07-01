9e812b95d8dbcd058297dc7dfe729ceeea97c661
Quan Chen <quanchen1986@gmail.com>
2015-07-01 15:38:25 -0400

    choose 100 inputs for each stage

--- im-mulage/find-input.sh
+++ im-mulage/find-input.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-#find /home/quan/PDA/PDAm/16k | xargs ls > wav.txt
-#find input -type f -size -1M > input.txt 
-find input -type f -size -1000000c > input.txt
+#find /home/quan/PDA/PDAm/16k -type f -name '*_5.wav' > wav.txt 
+while read line
+do
+	cp $line im-input/
+done < im.txt
+
+#find $PWD -type f > input.txt 
+#find input -type f > input.txt 
