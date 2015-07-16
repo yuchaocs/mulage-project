5c4c555351493747103b959cab48ac628e46c8d8
Thomas-Yang <hailong.yang1115@gmail.com>
2015-07-16 18:58:45 -0400

    update the command option for the launch script

--- commandcenter/start-cc-service.sh
+++ commandcenter/start-cc-service.sh
@@ -17,8 +17,10 @@ adjust_threshold=$5
 tail_percentile=$6
 gobal_power_budget=$7
 execution_mode=$8
+decision_policy=$9
+withdraw_instance=$10
 
 pinned_core=31
 
 taskset -c $pinned_core java -XX:+UseConcMarkSweepGC -server -Xms1024m -Xmx2048m \
- -jar commandcenter.jar $scheduler_port $adjust_interval $withdraw_interval $warm_up_account $adjust_threshold $tail_percentile $gobal_power_budget $execution_mode 2>&1 &
+ -jar commandcenter.jar $scheduler_port $adjust_interval $withdraw_interval $warm_up_account $adjust_threshold $tail_percentile $gobal_power_budget $execution_mode $decision_policy $withdraw_instance 2>&1 &
