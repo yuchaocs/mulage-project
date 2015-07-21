70f2b9cbfd8a083433b8549c95fc1db416837a16
Thomas-Yang <hailong.yang1115@gmail.com>
2015-07-21 10:49:44 -0400

    change back the short input list of asr

--- loadgen/start-stress-client.sh
+++ loadgen/start-stress-client.sh
@@ -12,8 +12,9 @@ distribution_file=$3
 query_num=$4
 warm_up_account=$5
 load_type=$6
+operation_type=$7
 
 pinned_core=30
 
 taskset -c $pinned_core java -XX:+UseConcMarkSweepGC -server -Xms1024m -Xmx2048m \
- -jar stressclient.jar $scheduler_ip $scheduler_port $distribution_file $query_num $warm_up_account $load_type 2>&1 &
+ -jar stressclient.jar $scheduler_ip $scheduler_port $distribution_file $query_num $warm_up_account $load_type $operation_type 2>&1 &
